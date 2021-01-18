994f7eabdea21c39444b99c06fb2704723a3dbb4
Feng Zhou <57497285+zhou-feng@users.noreply.github.com>
2021-01-18 13:45:37 +0800

    Add files via upload

--- ConvertDicomToBIDS_example.m
+++ ConvertDicomToBIDS_example.m
@@ -11,9 +11,7 @@ convertdcm2nii(dcmfiledir,outputdir,FuncSeriesDescription,AntSeriesDescription);
 %% STEP 2, check numbers of T1w and funct images for each subject
 % Of note, some subjects might have more imgs (e.g. due to re-scanning), which should be removed
 % make sure you have the same numbers of nT1w AND nFunct
-% the first column of nImgs is subID, the 2nd one is the num of T1w images
-% and the 3rd one is the number of functional images
-nImgs = checkimgs(outputdir);
+subinfo = checkimgs(outputdir);
 
 
 %% STEP 3, discard initial N volumes
@@ -22,7 +20,7 @@ discard_init_vols(outputdir,nVols);
 
 %% STEP 4, rename nii imgs to fit BIDS
 % left ones are part of the old names, 
-% right ones are the modified names (will automatically include subID)
+% right ones are the modified names (will automatically includes subID)
 functnames = {'*face_run_01*', 'face_run-01';...
 '*face_run_02*','face_run-02';...
 '*face_run_03*', 'face_run-03';...
@@ -35,37 +33,36 @@ renameBIDS(outputdir, functnames, T1wname);
 
 %% json files for each task (can obtain an initial one using dcm2niix)
 % the new version of dcm2niix can generate slice order for our GE scanner
-% you don't have to create the following slice order
 % but I think you have to include the TaskName
 
-% includes TaskName and slice timing
-% face_json = spm_jsonread('task-face_bold.json');
-% face_json.TaskName = 'face';
+dcmfile = spm_select; % select a dicom file for e.g. a task
+dcm2niipath = which('dcm2niix.exe');
+dcm2niiexe=['!' dcm2niipath];
+eval([dcm2niiexe, '  -b Y -z n -f "%i_%p" ',' -o ',datadir, ' ',dcmfile]);
+% you will find a json file in your datadir, rename it to fit your task
+% e.g. task-face_bold.json
+% includes TaskName
+face_json = spm_jsonread('task-face_bold.json');
+face_json.TaskName = 'face';
+spm_jsonwrite('task-face_bold.json', face_json, struct('indent', ' '));
+
+% in case you want to include slice timing info
 % timeperslice = 2/36;
 % order = 0:timeperslice:2;
 % slicetiming = zeros(36, 1);
 % slicetiming(1:2:end) = order(1:18);
 % slicetiming(2:2:end) = order(19:end-1);
 % face_json.SliceTiming = slicetiming;
-% spm_jsonwrite('task-face_bold.json', face_json, struct('indent', ' '));
-
-% IAPS_json = spm_jsonread('task-IAPS_bold.json');
-% IAPS_json.TaskName = 'IAPS';
-% timeperslice = 2/36;
-% order = 0:timeperslice:2;
-% slicetiming = zeros(36, 1);
-% slicetiming(1:2:end) = order(1:18);
-% slicetiming(2:2:end) = order(19:end-1);
-% IAPS_json.SliceTiming = slicetiming;
-% spm_jsonwrite('task-IAPS_bold.json', IAPS_json, struct('indent', ' '));
-
 %% dataset description
-% dd_json.Name = 'Zara Fear Dataset';
-% dd_json.BIDSVersion = '1.0.2';
-% dd_json.Authors = {'Weihua Zhao', 'Feng Zhou'};
-% json_options.indent = '  ';
-% jsonwrite('dataset_description.json', dd_json, json_options);
+dd_json.Name = 'Zara Fear Dataset';
+dd_json.BIDSVersion = '1.0.2';
+dd_json.Authors = {'Weihua Zhao', 'Feng Zhou'};
+json_options.indent = '  ';
+jsonwrite('dataset_description.json', dd_json, json_options);
  
 %% participant.tsv
-% sub = struct('participant_id', {{'120', '130'}}, 'sex', {{'M', 'F'}}, 'age', [20, 22]);
-% spm_save('participants.tsv', sub);
+subID{1,1} = '120'; subID{1,2} = '130';
+sex{1,1} = 'M'; sex{1,2} = 'F';
+age = [20, 22];
+sub = struct('participant_id', {subID}, 'sex', {sex}, 'age', age);
+spm_save('participants.tsv', sub);
